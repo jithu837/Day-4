509dd078ced292ccf2e1947ab4055db34de461ae
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-05 15:57:09 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class WhileLoop
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println(n++);
+        int a=4;
+        System.out.println(++a);
+        int b=6;
+        System.out.println(--b);
+        int c=56;
+        System.out.println(c--);
+    }
+}
