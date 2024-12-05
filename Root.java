f5cc6b97d078c057867fcb000e8fe1f0b08f1734
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-05 14:56:55 +0530

    Add files via upload

---
+++ Root.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Root
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=1;i*i<=n;i++)
+        {
+            System.out.println(i);
+
+        }
+    }
+}
