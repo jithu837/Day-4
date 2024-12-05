d5d0b84ae8147c00cd2cf46498378bef2ab32310
Jithendra Kumar <jithendrasolanki56@gmail.com>
2024-12-05 15:29:16 +0530

    Add files via upload

---
+++ PaperCutting.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class PaperCutting
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=(a/b)*(a/b);
+		    System.out.println(c);
+		}
+
+	}
+}
