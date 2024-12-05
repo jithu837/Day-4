f18bc6f11005516721c01f6ded696ec4ed370c02
jithu837 <jithendrasolanki56@gmail.com>
2024-12-05 12:03:50 +0530

    Add files via upload

---
+++ Multipleof8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ public class Multipleof8
+ {
+    public static void main(String args[])
+    {
+        int a=36;
+        if((a&5)==0)
+        {
+            System.out.println("Mutliple of 8");
+
+        }else
+        {
+            System.out.println("Not Multiple of 8"); 
+        }
+    } 
+ }
